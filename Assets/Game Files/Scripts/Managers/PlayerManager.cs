de18612721b2db38c4f6e34769113521e6d7c806
Christian <cjblack2013@gmail.com>
2022-07-15 15:01:50 -0500

    switch formations

--- Assets/Game Files/Scripts/Managers/PlayerManager.cs
+++ Assets/Game Files/Scripts/Managers/PlayerManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class PlayerManager : MonoBehaviour
 {
 	public static PlayerManager current;
+	[HideInInspector]
+	public PlayerController playerController;
 	public Vector2 input;
 	public int[] buttonBuffer;
 	public int[] buttonReleaseBuffer;
@@ -15,6 +17,9 @@ public class PlayerManager : MonoBehaviour
 
 	public int currentFormation;
 	public PlayerFormations[] PlayerFormations;
+
+	private float formationLockoutTimer = 0;
+	private float lockoutTime = 0.5f;
 
 	private void Start()
 	{
@@ -30,14 +35,140 @@ public class PlayerManager : MonoBehaviour
 		input = ctx.ReadValue<Vector2>();
 	}
 
-	public void BufferInput(int input)
+	public void BufferButton1(InputAction.CallbackContext ctx)
 	{
-		buttonBuffer[input] = 6;
+		if (ctx.performed)
+		{
+			buttonBuffer[0] = 6;
+		}
 	}
 
-	public void BufferRelease(int input)
+	public void BufferRelease1(InputAction.CallbackContext ctx)
 	{
-		buttonReleaseBuffer[input] = 6;
+		if (ctx.performed)
+		{
+			buttonReleaseBuffer[0] = 6;
+		}
+	}
+
+	public void BufferButton2(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			buttonBuffer[1] = 6;
+		}
+	}
+
+	public void BufferRelease2(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			buttonReleaseBuffer[1] = 6;
+		}
+	}
+
+	public void BufferButton3(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			buttonBuffer[2] = 6;
+		}
+	}
+
+	public void BufferRelease3(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			buttonReleaseBuffer[2] = 6;
+		}
+	}
+
+	public void BufferButton4(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			buttonBuffer[3] = 6;
+		}
+	}
+
+	public void BufferRelease4(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			buttonReleaseBuffer[3] = 6;
+		}
+	}
+
+	public void BufferButton5(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			buttonBuffer[4] = 6;
+		}
+	}
+
+	public void BufferRelease5(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			buttonReleaseBuffer[4] = 6;
+		}
+	}
+	public void OnFormationChange()
+	{
+		switch (PlayerFormations[currentFormation].ColliderShape)
+		{
+			case ColliderShape.Line:
+				{
+					break;
+				}
+			case ColliderShape.Triangle:
+				{
+					break;
+				}
+
+		}
+
+		playerController.LineCollider.gameObject.SetActive(PlayerFormations[currentFormation].ColliderShape == ColliderShape.Line);
+		playerController.TriangleCollider.gameObject.SetActive(PlayerFormations[currentFormation].ColliderShape == ColliderShape.Triangle);
+	}
+
+	public void SetFormation(int index)
+	{
+		currentFormation = index;
+
+		if (currentFormation >= PlayerFormations.Length)
+			currentFormation = 0;
+		if (currentFormation < 0)
+			currentFormation = PlayerFormations.Length - 1;
+
+		OnFormationChange();
+	}
+
+	public void IncrementFormation(int delta)
+	{
+		currentFormation += delta;
+
+		if (currentFormation >= PlayerFormations.Length)
+			currentFormation = 0;
+		if (currentFormation < 0)
+			currentFormation = PlayerFormations.Length-1;
+
+		OnFormationChange();
+	}
+
+	private void Update()
+	{
+		if (formationLockoutTimer > 0)
+			formationLockoutTimer -= Time.deltaTime;
+
+		if(buttonBuffer[4] > 0 && formationLockoutTimer <= 0f)
+		{
+			buttonBuffer[4] = 0;
+
+			formationLockoutTimer = lockoutTime;
+			IncrementFormation(1);
+		}
 	}
 }
 
@@ -46,6 +177,8 @@ public class PlayerManager : MonoBehaviour
 public class PlayerFormations
 {
 	public PlayerPositions[] playerPositions;
+	public ColliderShape ColliderShape;
+	public float ColliderRotation;
 }
 
 [System.Serializable]
@@ -53,4 +186,7 @@ public class PlayerPositions
 {
 	public Vector3 LocalOffset;
 	public Vector2 FacingDir;
+
 }
+
+public enum ColliderShape {Line, Triangle }
